ce168508b33e2ee93bdd2830fe4c36bd123651c8
nmm131 <nmm131@gmail.com>
2021-02-28 12:27:57 -0500

    Update main.tf, playbook-install-jenkins-kubernetes.yaml

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,9 +11,20 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Jenkins"
   }
+  connection {
+    type = var.connection_type
+    host = aws_instance.web.public_ip
+    user = var.connection_user
+    port = var.connection_port
+    private_key = file(var.connection_private_key)
+    agent = var.connection_agent
+  }
+  # Replace curl with ip of ec2-cluster
   provisioner "remote-exec" {
     inline = [
       "sudo amazon-linux-extras install ansible2 -y",
+      "sudo sh -c 'echo [ec2-cluster] >> /etc/ansible/hosts'",
+      "sudo sh -c 'echo `curl http://checkip.amazonaws.com` >> /etc/ansible/hosts'",
       "sudo yum install git -y",
       "git clone https://github.com/nmm131/terraform-aws-ansible-jenkins-k8-elastic-devops-pipeline.git /tmp/ansible-aws",
       "ansible-playbook /tmp/ansible-aws/ansible/playbook-install-jenkins-kubernetes.yaml"
