0281a65d272997c17dc26f667959c71dcb4d10c1
nmm131 <nmm131@gmail.com>
2021-03-01 09:43:52 -0500

    Update .gitignore, add variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "profile" {
+  type = string
+}
+variable "aws_region" {
+  type = string
+}
+variable "shared_credentials_file" {
+  type = string
+}
+variable "ami" {
+  type = string
+  sensitive = true
+}
+variable "instance_type" {
+  type = string
+}
+variable "key_name" {
+  type = string
+}
+variable "aws_security_groups" {
+  type = list
+}
+variable "connection_type" {
+  type = string
+}
+variable "connection_user" {
+  type = string
+}
+variable "connection_port" {
+  type = number
+}
+variable "connection_private_key" {
+  type = string
+}
+variable "connection_agent" {
+  type = bool
+}
+
